67cdfbce68b8e69de267771aec184efe3615b9c9
marco belahouane <nfk18@hotmail.co.uk>
2023-11-17 13:25:12 +0000

    CRUD functionality complete

--- pern-Todo/server/index.js
+++ pern-Todo/server/index.js
@@ -12,18 +12,58 @@ app.post("/todo", async(req, res)=>{
   try{
     const { description } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1)",
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
       [description]
     );
-    res.json(newTodo)
+    res.json(newTodo.rows[0])
   } catch(err){
     console.error(err.message)
   }
 })
 //get all
+app.get("/todo", async(req, res)=>{
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo")
+    res.json(allTodos.rows)
+  } catch (error) {
+    console.error(error.message)
+  }
+})
 //get a
+app.get("/todo/:id", async(req, res)=>{
+  try {
+    const {id} = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1",[id]);
+    res.json(todo.rows[0])
+  } catch (error) {
+      console.error(error.message)    
+  }
+})
 //update 
+app.put("/todo/:id", async(req, res)=>{
+  try {
+    const {id} = req.params;
+    const {description} = req.body;
+    const todoUpdate = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+    res.json("todo was updated")
+  } catch (error) {
+    console.error(error.message)
+  }
+ 
+})
 //delete
+app.delete("/todo/:id", async(req, res)=>{
+  try {
+    
+  
+  const {id} = req.params;
+  const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+  res.json('did it');
+} catch (error) {
+    console.error(error.message)
+}
+})
 app.listen(5000, ()=>{
   console.log("server has started on port 5000");
+  
 })
