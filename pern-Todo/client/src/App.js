1448546203e62da6bf2a31096fa37014c7d4152d
marco belahouane <nfk18@hotmail.co.uk>
2023-11-18 13:11:40 +0000

    react cret ed styling begun

---
+++ pern-Todo/client/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import './App.css';
+//components
+import InputTodo from './components/InputTodo';
+function App() {
+  return(
+<Fragment >
+<InputTodo/>
+</Fragment>
+  )
+}
+
+export default App;
---
+++ pern-Todo/client/src/components/InputTodo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function InputTodo() {
+  return (
+    <div class="top_div">
+    <h1 class="main_title">Todo List</h1>
+    <form class="top_form">
+      <input class="top_form_input"></input>
+      <button class="top_form_input_btn"></button>
+    </form>
+    </div>
+  )
+}
+
+export default InputTodo
